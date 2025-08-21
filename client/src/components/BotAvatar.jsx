c452e1f5747829478fd12ff711a6644ec4ad39d2
Towhidur Rahman <towhidasif@gmail.com>
2025-08-21 23:08:25 +0600

    Create a specific avatar component for AI responses.

---
+++ client/src/components/BotAvatar.jsx
@@ -0,0 +1,10 @@
+// BotAvatar.jsx
+import React from 'react';
+
+export const BotAvatar = () => {
+  return (
+    <div className="h-8 w-8">
+      <img className="p-1" src="/logo.png" alt="Bot Avatar" />
+    </div>
+  );
+};
