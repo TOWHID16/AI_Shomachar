0dc522583855bc08850dee3857d876a3485d10ab
Towhidur Rahman <towhidasif@gmail.com>
2025-08-21 22:50:45 +0600

    Create JWT verification middleware.

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const authMiddleware = (req, res, next) => {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // Attach user from the payload to the request object
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
+
+export default authMiddleware;
